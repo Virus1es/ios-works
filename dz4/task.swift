9a692b6d069317c5242a996a7ef34a1e5ae8a3f8
V1ruses <103028077+Virus1es@users.noreply.github.com>
2026-04-18 14:04:24 +0300

    upload 4th homework tasks

---
+++ dz4/task.swift
@@ -0,0 +1,17 @@
+// Встроенный метод
+let numbers = [2, 4, 6, 8]
+let builtIn = numbers.reduce(1) { $0 * $1 }
+print("Встроенный: \(builtIn)")
+
+// Собственная реализация
+func myReduceInt(_ array: [Int], _ initial: Int, _ combine: (Int, Int) -> Int) -> Int {
+    var accumulator = initial
+    for element in array {
+        accumulator = combine(accumulator, element)
+    }
+    return accumulator
+}
+
+// 3. Тест кастомной функции
+let custom = myReduceInt(numbers, 1) { acc, val in acc * val }
+print("Кастомный: \(custom)")
